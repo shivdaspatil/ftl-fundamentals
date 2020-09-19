4fe9d49c1b983d2614326e78567e4b0d8fab132b
Shivdas Patil <shivdas.patil@deskera.com>
2020-09-19 14:13:03 +0530

    Incorporate review comments and other fixes

--- calculator.go
+++ calculator.go
@@ -3,102 +3,54 @@ package calculator
 
 import (
 	"errors"
+	"fmt"
 	"math"
 	"strconv"
 	"strings"
 )
 
-// Add takes two numbers and returns the result of adding them together.
-func Add(a, b float64) float64 {
-	return a + b
-}
-
-// AddN takes a slice of numbers and returns the addition of the given numbers
-func AddN(numbers []float64) float64 {
-	result := 0.0
-	for _, n := range numbers {
+// Add takes two or more numbers and returns the result of adding them together.
+func Add(a, b float64, nums ...float64) float64 {
+	result := a + b
+	for _, n := range nums {
 		result += n
 	}
 	return result
 }
 
-// Subtract takes two numbers and returns the result of subtracting the second
+// Subtract takes two or more numbers and returns the result of subtracting the second
 // from the first.
-func Subtract(a, b float64) float64 {
-	return a - b
-}
-
-// SubtractN takes a slice of numbers and returns the subtraction of the given numbers
-// num1 - num2 - num3 etc..
-func SubtractN(numbers []float64) float64 {
-	length := len(numbers)
-
-	if length == 0 {
-		return 0
-	}
-	if length == 1 {
-		return numbers[0]
-	}
-
-	result := numbers[0]
-	for i := 1; i < length; i++ {
-		result -= numbers[i]
+func Subtract(a, b float64, nums ...float64) float64 {
+	result := a - b
+	for _, n := range nums {
+		result -= n
 	}
 	return result
 }
 
-// Multiply takes two numbers and returns the result of multiplication of both
+// Multiply takes two or more numbers and returns the result of multiplication of both
 // the numbers
-func Multiply(a, b float64) float64 {
-	return a * b
-}
-
-// MultiplyN takes a slice of numbers and returns result of multiplication of
-// all the numbers in the slice
-func MultiplyN(numbers []float64) float64 {
-	length := len(numbers)
-
-	if length == 0 {
-		return 0
-	}
-	if length == 1 {
-		return numbers[0]
-	}
-
-	result := numbers[0]
-	for i := 1; i < length; i++ {
-		result *= numbers[i]
+func Multiply(a, b float64, nums ...float64) float64 {
+	result := a * b
+	for _, n := range nums {
+		result *= n
 	}
 	return result
 }
 
-// Divide takes two numbers and returns the result of dividing the first number
-// by second number. It returns error if the division operation is not valid
-func Divide(a, b float64) (float64, error) {
+// Divide takes two or more numbers and returns the result of dividing the given numbers.
+// It returns error if the division operation is not valid
+func Divide(a, b float64, nums ...float64) (float64, error) {
 	if b == 0 {
 		return 0, errors.New("Divide by Zero")
 	}
-	return a / b, nil
-}
+	result := a / b
 
-// DivideN takes a slice of numbers and returns the division of the given numbers
-// ((num1 / num2) / num3) / num4 etc..
-func DivideN(numbers []float64) (float64, error) {
-	length := len(numbers)
-
-	if length == 0 {
-		return 0, nil
-	}
-	if length == 1 {
-		return numbers[0], nil
-	}
-
-	result := numbers[0]
-	for i := 1; i < length; i++ {
-		if numbers[i] == 0 {
-			return 0, errors.New("Divide by 0")
+	for _, n := range nums {
+		if n == 0 {
+			return 0, errors.New("Divide by Zero")
 		}
-		result /= numbers[i]
+		result /= n
 	}
 	return result, nil
 }
@@ -107,7 +59,7 @@ func DivideN(numbers []float64) (float64, error) {
 // number is invalid
 func SquareRoot(a float64) (float64, error) {
 	if a < 0 {
-		return 0, errors.New("negative numbers don't have square root")
+		return 0, fmt.Errorf("bad input %f; square root is undefined for negative values", a)
 	}
 	return math.Sqrt(a), nil
 }
@@ -116,37 +68,35 @@ func SquareRoot(a float64) (float64, error) {
 // and returns the result of that operation. Returns error if the string is not in
 // the acceptable form.
 func ComputeString(str string) (float64, error) {
-	for _, op := range "+-*/" {
-		if !strings.ContainsAny(str, string(op)) {
-			continue
-		}
 
-		elements := strings.Split(str, string(op))
-
-		if len(elements) != 2 {
-			return 0, errors.New("did not get two operands")
-		}
-
-		num1, err := strconv.ParseFloat(strings.TrimSpace(elements[0]), 64)
-		if err != nil {
-			return 0, errors.New("error parsing the first number")
-		}
-
-		num2, err := strconv.ParseFloat(strings.TrimSpace(elements[1]), 64)
-		if err != nil {
-			return 0, errors.New("error parsing the second number")
-		}
-
-		switch op {
-		case '+':
-			return num1 + num2, nil
-		case '-':
-			return num1 - num2, nil
-		case '*':
-			return num1 * num2, nil
-		case '/':
-			return num1 / num2, nil
-		}
+	opIndex := strings.IndexAny(str, "+-*/")
+	if opIndex == -1 {
+		return 0, errors.New("Invalid string")
 	}
-	return 0, errors.New("invalid input string")
+
+	operand1 := strings.TrimSpace(str[0:opIndex])
+	operand2 := strings.TrimSpace(str[opIndex+1:])
+
+	num1, err := strconv.ParseFloat(operand1, 64)
+	if err != nil {
+		return 0, errors.New("error parsing the first number")
+	}
+
+	num2, err := strconv.ParseFloat(operand2, 64)
+	if err != nil {
+		return 0, errors.New("error parsing the second number")
+	}
+
+	switch str[opIndex] {
+	case '+':
+		return num1 + num2, nil
+	case '-':
+		return num1 - num2, nil
+	case '*':
+		return num1 * num2, nil
+	case '/':
+		return num1 / num2, nil
+	default:
+		return 0, errors.New("Unsupported operator")
+	}
 }
--- calculator_test.go
+++ calculator_test.go
@@ -8,99 +8,72 @@ import (
 	"time"
 )
 
-func almostEqual(a, b float64) bool {
-	const float64EqualityThreshold = 1e-9
-	return math.Abs(a-b) <= float64EqualityThreshold
+func closeEnough(a, b float64) bool {
+	const epsilon = 1e-9
+	return math.Abs(a-b) <= epsilon
 }
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var tcs = []struct {
+	tcs := []struct {
 		name string
-		want float64
 		a    float64
 		b    float64
+		nums []float64
+		want float64
 	}{
 		{
 			name: "Two positive numbers",
-			want: 5,
 			a:    3,
 			b:    2,
+			want: 5,
 		},
 		{
 			name: "Zero subtracted with any number",
-			want: -2,
 			a:    0,
 			b:    -2,
+			want: -2,
 		},
 		{
 			name: "Positive and negative number",
-			want: -1,
 			a:    -5,
 			b:    4,
+			want: -1,
 		},
 		{
 			name: "Two negative numbers",
-			want: -11,
 			a:    -8,
 			b:    -3,
+			want: -11,
 		},
 		{
 			name: "Two fractional numbers",
-			want: 6.5,
 			a:    3.3,
 			b:    3.2,
+			want: 6.5,
+		},
+		{
+			name: "More than two negative numbers",
+			a:    -8,
+			b:    -3,
+			nums: []float64{-1, -2, -3},
+			want: -17,
+		},
+
+		{
+			name: "More than two fractional numbers",
+			a:    3.3,
+			b:    3.2,
+			nums: []float64{2.1},
+			want: 8.6,
 		},
 	}
 
 	for _, tc := range tcs {
-		got := calculator.Add(tc.a, tc.b)
-		if !almostEqual(tc.want, got) {
-			t.Errorf("Test Add(%f, %f) (%s) failed: want %f, got %f",
-				tc.a, tc.b, tc.name, tc.want, got)
-		}
-	}
-}
-
-func TestAddN(t *testing.T) {
-	t.Parallel()
-	var tcs = []struct {
-		name string
-		want float64
-		nums []float64
-	}{
-		{
-			name: "Two positive numbers",
-			want: 3,
-			nums: []float64{1, 2},
-		},
-		{
-			name: "Single number in slice ",
-			want: 5,
-			nums: []float64{5},
-		},
-		{
-			name: "Empty Slice ",
-			want: 0,
-			nums: []float64{},
-		},
-		{
-			name: "More than 2 numbers",
-			want: 10,
-			nums: []float64{1, 2, 3, 4},
-		},
-		{
-			name: "More than 2 fractions",
-			want: 10.6,
-			nums: []float64{1.1, 2.2, 3.1, 4.2},
-		},
-	}
-
-	for _, tc := range tcs {
-		got := calculator.AddN(tc.nums)
-		if !almostEqual(tc.want, got) {
-			t.Errorf("Test AddN(%v) (%s) failed: want %f, got %f",
-				tc.nums, tc.name, tc.want, got)
+		got := calculator.Add(tc.a, tc.b, tc.nums...)
+		if !closeEnough(tc.want, got) {
+			t.Errorf("%s : Add(%f, %f, %v) : want %f, got %f",
+				tc.name, tc.a, tc.b, tc.nums, tc.want, got)
 		}
 	}
 }
@@ -114,354 +87,256 @@ func TestAddRandom(t *testing.T) {
 		want := randA + randB
 		got := calculator.Add(randA, randB)
 		if want != got {
-			t.Errorf("want %f, got %f", want, got)
+			t.Errorf("(Add random numbers): Add(%f, %f) : want %f, got %f",
+				randA, randB, want, got)
 		}
 	}
 }
 
 func TestSubtract(t *testing.T) {
 	t.Parallel()
-	var tcs = []struct {
+	tcs := []struct {
 		name string
-		want float64
 		a    float64
 		b    float64
+		nums []float64
+		want float64
 	}{
 		{
 			name: "Two positive numbers",
-			want: 3,
 			a:    5,
 			b:    2,
+			want: 3,
 		},
 		{
 			name: "Zero subtracted with any number",
-			want: -2,
 			a:    0,
 			b:    2,
+			want: -2,
 		},
 		{
-			name: "Positive and negative number",
-			want: -9,
+			name: "Positive number subtracted from negative number",
 			a:    -5,
 			b:    4,
+			want: -9,
 		},
 		{
 			name: "Two negative numbers",
-			want: -5,
 			a:    -8,
 			b:    -3,
+			want: -5,
 		},
 		{
 			name: "Two fractional numbers",
-			want: 0.1,
 			a:    3.3,
 			b:    3.2,
-		},
-	}
-
-	for _, tc := range tcs {
-		got := calculator.Subtract(tc.a, tc.b)
-		if !almostEqual(tc.want, got) {
-			t.Errorf("Test Subtract(%f, %f) (%s) failed: want %f, got %f",
-				tc.a, tc.b, tc.name, tc.want, got)
-		}
-	}
-}
-
-func TestSubtractN(t *testing.T) {
-	t.Parallel()
-	var tcs = []struct {
-		name string
-		want float64
-		nums []float64
-	}{
-		{
-			name: "Two positive numbers",
-			want: -1,
-			nums: []float64{1, 2},
+			want: 0.1,
 		},
 		{
-			name: "Single number in slice ",
-			want: 5,
-			nums: []float64{5},
-		},
-		{
-			name: "Empty Slice ",
-			want: 0,
-			nums: []float64{},
-		},
-		{
-			name: "More than 2 numbers",
-			want: -8,
-			nums: []float64{1, 2, 3, 4},
+			name: "More than two negative numbers",
+			a:    -8,
+			b:    -3,
+			nums: []float64{-1, -5, -10, -4},
+			want: 15,
 		},
 		{
 			name: "More than 2 fractions",
-			want: 2.2,
+			a:    3.3,
+			b:    3.2,
 			nums: []float64{5.5, 1.1, 2.2},
+			want: -8.7,
 		},
 	}
 
 	for _, tc := range tcs {
-		got := calculator.SubtractN(tc.nums)
-		if !almostEqual(tc.want, got) {
-			t.Errorf("Test SubtractN(%v) (%s) failed: want %f, got %f",
-				tc.nums, tc.name, tc.want, got)
+		got := calculator.Subtract(tc.a, tc.b, tc.nums...)
+		if !closeEnough(tc.want, got) {
+			t.Errorf("%s : Subtract(%f, %f, %v) : want %f, got %f",
+				tc.name, tc.a, tc.b, tc.nums, tc.want, got)
 		}
 	}
 }
 
 func TestMultiply(t *testing.T) {
 	t.Parallel()
-	var tcs = []struct {
+	tcs := []struct {
 		name string
-		want float64
 		a    float64
 		b    float64
+		nums []float64
+		want float64
 	}{
 		{
 			name: "Two positive numbers",
-			want: 10,
 			a:    5,
 			b:    2,
+			want: 10,
 		},
 		{
 			name: "Zero multiplied with any number",
-			want: 0,
 			a:    0,
 			b:    2,
+			want: 0,
 		},
 		{
 			name: "Positive and negative number",
-			want: -20,
 			a:    -5,
 			b:    4,
+			want: -20,
 		},
 		{
 			name: "Two negative numbers",
-			want: 24,
 			a:    -8,
 			b:    -3,
+			want: 24,
 		},
 		{
 			name: "Two fractional numbers",
-			want: 4.03,
 			a:    1.3,
 			b:    3.1,
-		},
-	}
-
-	for _, tc := range tcs {
-		got := calculator.Multiply(tc.a, tc.b)
-		if !almostEqual(tc.want, got) {
-			t.Errorf("Test Multiply(%f, %f) (%s) failed: want %f, got %f",
-				tc.a, tc.b, tc.name, tc.want, got)
-		}
-	}
-}
-
-func TestMultiplyN(t *testing.T) {
-	t.Parallel()
-	var tcs = []struct {
-		name string
-		want float64
-		nums []float64
-	}{
-		{
-			name: "Two positive numbers",
-			want: 10,
-			nums: []float64{5, 2},
-		},
-		{
-			name: "Single number in slice ",
-			want: 5,
-			nums: []float64{5},
-		},
-		{
-			name: "Empty Slice ",
-			want: 0,
-			nums: []float64{},
+			want: 4.03,
 		},
 		{
 			name: "More than 2 numbers",
-			want: 24,
-			nums: []float64{1, 2, 3, 4},
+			a:    2,
+			b:    3,
+			nums: []float64{2, 4},
+			want: 48,
 		},
 		{
-			name: "More than 2 fractions",
+			name: "More than two fractional numbers",
+			a:    5.5,
+			b:    1.1,
+			nums: []float64{2.2},
 			want: 13.31,
-			nums: []float64{5.5, 1.1, 2.2},
 		},
 	}
 
 	for _, tc := range tcs {
-		got := calculator.MultiplyN(tc.nums)
-		if !almostEqual(tc.want, got) {
-			t.Errorf("Test MultiplyN(%v) (%s) failed: want %f, got %f",
-				tc.nums, tc.name, tc.want, got)
+		got := calculator.Multiply(tc.a, tc.b, tc.nums...)
+		if !closeEnough(tc.want, got) {
+			t.Errorf("%s: Multiply(%f, %f, %v) : want %f, got %f",
+				tc.name, tc.a, tc.b, tc.nums, tc.want, got)
 		}
 	}
 }
 
 func TestDivide(t *testing.T) {
 	t.Parallel()
-	var tcs = []struct {
+	tcs := []struct {
 		name        string
-		want        float64
 		a           float64
 		b           float64
+		nums        []float64
+		want        float64
 		errExpected bool
 	}{
 		{
 			name:        "Two positive numbers",
-			want:        5,
 			a:           10,
 			b:           2,
+			want:        5,
 			errExpected: false,
 		},
 		{
 			name:        "Zero divided with any number",
-			want:        0,
 			a:           0,
 			b:           2,
+			want:        0,
 			errExpected: false,
 		},
 		{
 			name:        "Positive and negative number",
-			want:        -1.25,
 			a:           -5,
 			b:           4,
+			want:        -1.25,
 			errExpected: false,
 		},
 		{
 			name:        "Two negative numbers",
-			want:        4,
 			a:           -8,
 			b:           -2,
+			want:        4,
 			errExpected: false,
 		},
 		{
 			name:        "Two fractional numbers",
-			want:        2.75,
 			a:           3.3,
 			b:           1.2,
+			want:        2.75,
 			errExpected: false,
 		},
 		{
 			name:        "Divide by zero",
-			want:        0,
 			a:           2,
 			b:           0,
-			errExpected: true,
-		},
-	}
-
-	for _, tc := range tcs {
-		got, err := calculator.Divide(tc.a, tc.b)
-		errReceived := err != nil
-		if tc.errExpected != errReceived {
-			t.Errorf("Test Divide (%f, %f) (%s) failed: Unexpected error received",
-				tc.a, tc.b, tc.name)
-		} else {
-			if !tc.errExpected && !almostEqual(tc.want, got) {
-				t.Errorf("Test Divide (%f, %f) (%s) failed: want %f, got %f",
-					tc.a, tc.b, tc.name, tc.want, got)
-			}
-		}
-	}
-}
-
-func TestDivideN(t *testing.T) {
-	t.Parallel()
-	var tcs = []struct {
-		name        string
-		want        float64
-		nums        []float64
-		errExpected bool
-	}{
-		{
-			name:        "Two positive numbers",
-			want:        5,
-			nums:        []float64{10, 2},
-			errExpected: false,
-		},
-		{
-			name:        "Single number in slice ",
-			want:        5,
-			nums:        []float64{5},
-			errExpected: false,
-		},
-		{
-			name:        "Empty Slice ",
 			want:        0,
-			nums:        []float64{},
-			errExpected: false,
+			errExpected: true,
 		},
 		{
 			name:        "More than 2 numbers",
-			want:        1,
-			nums:        []float64{20, 5, 2, 2},
-			errExpected: false,
-		},
-		{
-			name:        "More than 2 fractions",
+			a:           40,
+			b:           5,
+			nums:        []float64{2, 2},
 			want:        2,
-			nums:        []float64{5.5, 1.1, 2.5},
-			errExpected: false,
-		},
-		{
-			name:        "More than 2 fractions starting with 0",
-			want:        0,
-			nums:        []float64{0, 1.1, 2.5},
 			errExpected: false,
 		},
 		{
 			name:        "More than 2 fractions",
+			a:           5.5,
+			b:           1.1,
+			nums:        []float64{0.5, 0.2, 2.5},
+			want:        20,
+			errExpected: false,
+		},
+		{
+			name:        "Divide by 0 in slice",
+			a:           20,
+			b:           2,
+			nums:        []float64{2, 0},
 			want:        0,
-			nums:        []float64{5.5, 0, 2.2},
 			errExpected: true,
 		},
 	}
 
 	for _, tc := range tcs {
-		got, err := calculator.DivideN(tc.nums)
+		got, err := calculator.Divide(tc.a, tc.b, tc.nums...)
 		errReceived := err != nil
 		if tc.errExpected != errReceived {
-			t.Errorf("Test DivideN (%v) (%s) failed: Unexpected error received",
-				tc.nums, tc.name)
-		} else {
-			if !tc.errExpected && !almostEqual(tc.want, got) {
-				t.Errorf("Test DivideN (%v) (%s) failed: want %f, got %f",
-					tc.nums, tc.name, tc.want, got)
-			}
+			t.Errorf("%s: Divide (%f, %f, %v) : Unexpected error received",
+				tc.name, tc.a, tc.b, tc.nums)
+		}
+		if !tc.errExpected && !closeEnough(tc.want, got) {
+			t.Errorf("%s: Divide(%f, %f, %v) : want %f, got %f",
+				tc.name, tc.a, tc.b, tc.nums, tc.want, got)
 		}
 	}
 }
 
 func TestSquareRoot(t *testing.T) {
 	t.Parallel()
-	var tcs = []struct {
+	tcs := []struct {
 		name        string
-		want        float64
 		a           float64
+		want        float64
 		errExpected bool
 	}{
 		{
 			name:        "Squareroot of positive number",
-			want:        4,
 			a:           16,
+			want:        4,
 			errExpected: false,
 		},
 		{
 			name:        "Squareroot of 0",
-			want:        0,
 			a:           0,
+			want:        0,
 			errExpected: false,
 		},
 		{
 			name:        "Squareroot of negative number",
-			want:        0,
 			a:           -1,
+			want:        0,
 			errExpected: true,
 		},
 	}
@@ -470,20 +345,19 @@ func TestSquareRoot(t *testing.T) {
 		got, err := calculator.SquareRoot(tc.a)
 		errReceived := err != nil
 		if tc.errExpected != errReceived {
-			t.Errorf("Test Squareroot (%f) (%s) failed: Unexpected error received",
-				tc.a, tc.name)
-		} else {
-			if !tc.errExpected && !almostEqual(tc.want, got) {
-				t.Errorf("Test Squareroot (%f) (%s) failed: want %f, got %f",
-					tc.a, tc.name, tc.want, got)
-			}
+			t.Errorf("%s: Squareroot(%f) : Unexpected error received",
+				tc.name, tc.a)
+		}
+		if !tc.errExpected && !closeEnough(tc.want, got) {
+			t.Errorf("%s: Squareroot(%f) : want %f, got %f",
+				tc.name, tc.a, tc.want, got)
 		}
 	}
 }
 
 func TestComputeString(t *testing.T) {
 	t.Parallel()
-	var tcs = []struct {
+	tcs := []struct {
 		name        string
 		input       string
 		want        float64
@@ -491,12 +365,12 @@ func TestComputeString(t *testing.T) {
 	}{
 		{
 			name:        "Addition of two numbers with no spaces",
-			input:       "2*2",
+			input:       "2+2",
 			want:        4,
 			errExpected: false,
 		},
 		{
-			name:        "Subtraction with leading space",
+			name:        "Subtraction with embedded space",
 			input:       "10     -     2",
 			want:        8,
 			errExpected: false,
@@ -514,7 +388,7 @@ func TestComputeString(t *testing.T) {
 			errExpected: false,
 		},
 		{
-			name:        "No operator",
+			name:        "Unsupported operator",
 			input:       "2%5",
 			want:        0,
 			errExpected: true,
@@ -531,17 +405,23 @@ func TestComputeString(t *testing.T) {
 			want:        0,
 			errExpected: true,
 		},
+		{
+			name:        "No operator ",
+			input:       "2 2",
+			want:        0,
+			errExpected: true,
+		},
 	}
 
 	for _, tc := range tcs {
 		got, err := calculator.ComputeString(tc.input)
 		errReceived := err != nil
 		if tc.errExpected != errReceived {
-			t.Errorf("Test ComputeString (%s) (%s) failed: Unexpected error received",
+			t.Errorf("%s: ComputeString(%s) : Unexpected error received",
 				tc.input, tc.name)
 		} else {
-			if !tc.errExpected && !almostEqual(tc.want, got) {
-				t.Errorf("Test ComputeString (%s) (%s) failed: want %f, got %f",
+			if !tc.errExpected && !closeEnough(tc.want, got) {
+				t.Errorf("%s: ComputeString (%s) : want %f, got %f",
 					tc.input, tc.name, tc.want, got)
 			}
 		}
